86646654ca59e34c3edba7c150c336d48e6084d1
nguyenngocanlc <nguyenngocanlc@gmail.com>
2022-11-18 14:42:28 +0700

    update component profile

--- DemoAngular/src/app/profile/profile.component.ts
+++ DemoAngular/src/app/profile/profile.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  fullName: string = 'Ấn';
+  dobYear: number = 1989;
+  mobile: string = '0914 126 849';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
